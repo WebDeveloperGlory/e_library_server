6f58bddd13f17177cae38700fc590eb847e33f2d
WebDeveloperGlory <gloryumuerri223@gmail.com>
2023-03-07 04:50:38 -0800

    working mongodb atlas

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 //Set up default mongoose connection
 var mongoDB = 'mongodb://127.0.0.1/e_library';
-// var mongoDB = 'mongodb+srv://gloryumuerri223:Web351Developer.72@cluster0.kj1tjrm.mongodb.net/e_lib_server?retryWrites=true&w=majority';
-mongoose.connect(mongoDB, { useNewUrlParser: true }).then(() => console.log("connected to mongoDB atlas"));
+var mongoDB_URL = 'mongodb+srv://gloryumuerri223:Web351Developer.72@cluster0.kj1tjrm.mongodb.net/e_lib_server?retryWrites=true&w=majority';
+mongoose.connect(mongoDB_URL, { useNewUrlParser: true }).then(() => console.log("connected to mongoDB atlas"));
  //Get the default connection
 var db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
