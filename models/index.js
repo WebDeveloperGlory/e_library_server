82af92c2053f2fb6b9398b4dd766377f780078fa
WebDeveloperGlory <gloryumuerri223@gmail.com>
2023-03-08 03:27:45 -0800

    add offline db for testing

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 //Set up default mongoose connection
 var mongoDB = 'mongodb://127.0.0.1/e_library';
 var mongoDB_URL = 'mongodb+srv://gloryumuerri223:Web351Developer.72@cluster0.kj1tjrm.mongodb.net/e_lib_server?retryWrites=true&w=majority';
-mongoose.connect(mongoDB_URL, { useNewUrlParser: true }).then(() => console.log("connected to mongoDB atlas"));
+mongoose.connect(mongoDB_URL || mongoDB, { useNewUrlParser: true }).then(() => console.log("connected to mongoDB atlas"));
  //Get the default connection
 var db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
